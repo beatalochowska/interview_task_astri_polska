1b20dfe95e1f7fa14f932637b83ad47e364827f9
beatalochowska <beata.lochowska@gmail.com>
2020-01-19 19:23:44 +0100

    point 4 works

--- index.js
+++ index.js
@@ -1,69 +1,82 @@
 import "ol/ol.css";
-import { Map, View } from "ol";
+import Map from "ol/Map";
+import View from "ol/View";
+import { toLonLat } from "ol/proj";
+
+import Draw from "ol/interaction/Draw";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
 import { OSM, Vector as VectorSource } from "ol/source";
-import { defaults as defaultControls, ScaleLine } from "ol/control";
-import TileWMS from "ol/source/TileWMS";
-import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
-import { Draw, Snap } from "ol/interaction";
 
-const layers = [
-  new TileLayer({
-    source: new TileWMS({
-      url: "https://ahocevar.com/geoserver/wms",
-      params: {
-        LAYERS: "ne:NE1_HR_LC_SR_W_DR",
-        TILED: true
-      }
-    })
-  })
-];
+import { defaults as defaultControls } from "ol/control";
+import MousePosition from "ol/control/MousePosition";
+import { createStringXY } from "ol/coordinate";
+
+const mousePositionControl4326 = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: "EPSG:4326",
+  // comment the following two lines to have the mouse position
+  // be placed within the map.
+  className: "custom-mouse-position",
+  target: document.getElementById("EPSG:4326"),
+  undefinedHTML: "&nbsp;"
+});
+
+const mousePositionControl3857 = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: "EPSG:3857",
+  // comment the following two lines to have the mouse position
+  // be placed within the map.
+  className: "custom-mouse-position",
+  target: document.getElementById("EPSG:3857"),
+  undefinedHTML: "&nbsp;"
+});
 
 const raster = new TileLayer({
   source: new OSM()
 });
 
-var source = new VectorSource();
-var vector = new VectorLayer({
-  source: source,
-  style: new Style({
-    fill: new Fill({
-      color: "rgba(255, 255, 255, 0.2)"
-    }),
-    image: new CircleStyle({
-      radius: 7,
-      fill: new Fill({
-        color: "#ffcc33"
-      })
-    })
-  })
+const source = new VectorSource({ wrapX: false });
+
+const vector = new VectorLayer({
+  source: source
 });
 
 const map = new Map({
   controls: defaultControls().extend([
-    new ScaleLine({
-      units: "degrees"
-    })
+    mousePositionControl4326,
+    mousePositionControl3857
   ]),
   layers: [raster, vector],
   target: "map",
   view: new View({
-    projection: "EPSG:3857",
     center: [0, 0],
     zoom: 2
   })
 });
 
-var draw, snap; // global so we can remove them later
+let draw; // global so we can remove it later
 
-function addInteractions() {
+map.on("click", function(evt) {
+  const coords = toLonLat(evt.coordinate);
+  console.log(evt.coordinate);
+  const lat4326 = evt.coordinate[1];
+  const lon4326 = evt.coordinate[0];
+  const lat3857 = coords[1];
+  const lon3857 = coords[0];
+  const locTxt4326 = "Latitude: " + lat4326 + " Longitude: " + lon4326;
+  const locTxt3857 = "Latitude: " + lat3857 + " Longitude: " + lon3857;
+  // coords is a div in HTML below the map to display
+  document.getElementById("clickedPoint3857").innerHTML = locTxt3857;
+  document.getElementById("clickedPoint4326").innerHTML = locTxt4326;
+});
+
+function addInteraction() {
   draw = new Draw({
     source: source,
     type: "Point"
   });
+
   map.addInteraction(draw);
-  snap = new Snap({ source: source });
-  map.addInteraction(snap);
 }
 
-addInteractions();
+addInteraction();
